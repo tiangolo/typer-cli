c860b32728872922566055ca0e73b7cd44907a34
Sebastián Ramírez <tiangolo@gmail.com>
2020-03-09 22:00:50 +0100

    :white_check_mark: Add tests for docs

--- tests/test_doc.py
+++ tests/test_doc.py
@@ -24,6 +24,33 @@ def test_doc():
     assert docs in result.stdout
 
 
+def test_doc_output(tmp_path: Path):
+    out_file: Path = tmp_path / "out.md"
+    result = subprocess.run(
+        [
+            "coverage",
+            "run",
+            "-m",
+            "typer_cli",
+            "tests.assets.multi_app",
+            "utils",
+            "docs",
+            "--name",
+            "multiapp",
+            "--output",
+            str(out_file),
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf-8",
+    )
+    docs_path: Path = Path(__file__).parent / "assets/multiapp-docs.md"
+    docs = docs_path.read_text()
+    written_docs = out_file.read_text()
+    assert docs in written_docs
+    assert "Docs saved to:" in result.stdout
+
+
 def test_doc_not_existing():
     result = subprocess.run(
         ["coverage", "run", "-m", "typer_cli", "no_typer", "utils", "docs"],
