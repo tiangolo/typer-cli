adc1fb25415b8e8ae96477463c3daa1425c7cf76
Sebastián Ramírez <tiangolo@gmail.com>
2020-03-16 15:36:14 +0100

    :wrench: Update lint config

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,6 +3,6 @@
 set -e
 set -x
 
-mypy typer_cli --disallow-untyped-defs
+mypy typer_cli
 black typer_cli tests --check
 isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --check-only --thirdparty typer_cli typer_cli tests
